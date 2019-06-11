461aebd0d537a4b91f98185d6853e6f224f23716
Szymon <sz.kwasiborski@gmail.com>
2019-06-11 11:41:46 +0200

    Pierwsza Poprawka+tekstury

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ private:
     float vy=-200;
 
 public:
-    kulka():sf::CircleShape(){};
+    kulka():sf::CircleShape(){}
 
     sf::FloatRect get_bounds(){
                 return getGlobalBounds();
@@ -56,10 +56,10 @@ public:
 class player:public sf::Sprite
 {
 private:
-    float v_y_=20.0;
-    float v_x_=20.0;
+    float v_y_;
+    float v_x_;
 public:
-    player():sf::Sprite(){};
+    player():sf::Sprite(){}
 
     void setvx(float vx)
     {
@@ -121,6 +121,7 @@ public:
 
 int main()
 {
+    int lvl =0;
 
 
 
@@ -131,11 +132,6 @@ int main()
             std::cerr << "Could not load texture" << std::endl;
             return 1;
         }
-    sf::Texture texture1;
-          if (!texture1.loadFromFile("tlo.jpg")) {
-                std::cerr << "Could not load texture" << std::endl;
-                return 1;
-            }
           sf::Texture texture2;
                 if (!texture2.loadFromFile("Meta.png")) {
                       std::cerr << "Could not load texture" << std::endl;
@@ -146,7 +142,7 @@ int main()
     vector<kulka> sc;
 
     kulka s1;
-    kulka s2;
+
 
 
     for(int i=0;i<8;i++)
@@ -160,13 +156,15 @@ int main()
     for(int i=0;i<8;i++)
         {
 
-        s2.setvxs(200);
-        s2.setvys(0);
-        s2.setRadius(15);
-        s2.setPosition(-50,200+i*80);
-        s2.setFillColor(sf::Color(10,20,200));
-        sc.emplace_back(s2);
+        s1.setvxs(200);
+        s1.setvys(0);
+        s1.setRadius(15);
+        s1.setPosition(-50,200+i*80);
+        s1.setFillColor(sf::Color(10,20,200));
+        sc.emplace_back(s1);
         }
+
+
 
 
 
@@ -182,8 +180,10 @@ int main()
     winsquare.setTexture(texture2);
     winsquare.setPosition(720,520);
 
-    sf::Sprite tlogry;
-    tlogry.setTexture(texture1);
+
+    sf::RectangleShape tlogry;
+    tlogry.setSize(sf::Vector2f(1000,1000));
+    tlogry.setFillColor(sf::Color(150,255,0));
 
 
 
@@ -197,34 +197,76 @@ int main()
             }
 
             window.clear(sf::Color::Black);
-            window.draw(tlogry);
-            window.draw(winsquare);
-            window.draw(gracz);
-            for(int i=0;i<sc.size();i++)
-            {
-                window.draw(sc[i]);
-                sc[i].stepk(delta);
-                if(gracz.win(winsquare)==true)
-                {
-                 gracz.setPosition(0,560);
-                 winsquare.setPosition(720,0);
-                 for(int j=0;j<sc.size();j++){
-                 sc[j].setvxs(200);
-                 sc[j].setvys(0);
-                 sc[j].setPosition(-50+j*80,0+j*80);
 
+
+
+            if(winsquare.getGlobalBounds().intersects(gracz.getGlobalBounds()))
+            {
+            lvl++;
+            gracz.setPosition(0,0);
+            for (int i=0;i!=sc.size();i++) {
+                sc.erase(sc.begin()+i);
+            }
+            }
+            if(lvl==0)
+            {
+
+                window.draw(tlogry);
+                window.draw(winsquare);
+                window.draw(gracz);
+                for(int i=0;i<sc.size();i++)
+                {
+                    window.draw(sc[i]);
+                    sc[i].stepk(delta);
                 }
-                 if(gracz.win(winsquare)==true)
-                  {
-                      window.close();
-                  }
-                }
+                gracz.step(delta,gracz,sc);
 
             }
 
 
+            if(lvl==1)
+            {
+                window.draw(tlogry);
+                window.draw(winsquare);
+                window.draw(gracz);
 
-            gracz.step(delta,gracz,sc);
+
+                winsquare.setPosition(720,0);
+                for(int i=0;i<3;i++)
+                    {
+                    s1.setvxs(20);
+                    s1.setvys(10);
+                    s1.setRadius(35);
+                    s1.setPosition(-50+i*20,200+i*80);
+                    s1.setFillColor(sf::Color(10,20,200));
+                    sc.emplace_back(s1);
+                    }
+                for(int j=0;j!=sc.size();j++){
+                window.draw(sc[j]);
+                sc[j].setvxs(200);
+                sc[j].setvys(0);
+                sc[j].setPosition(-50+j*80,0+j*80);
+                sc[j].stepk(delta);
+                }
+
+
+
+                gracz.step(delta,gracz,sc);
+
+
+            }
+
+            if(lvl==2)
+            {
+                window.close();
+            }
+
+
+
+
+
+
+
 
 
 
